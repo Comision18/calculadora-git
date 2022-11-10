06927ccc1dcd80d3fd4a2a250b4fda8b9682baf0
Eric <menaericdaniel@gmail.com>
2022-11-10 10:06:07 -0300

    crea logica en entry-point

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const {sumar,restar,multiplicar,dividir} = require('./modules/calculadora');
+const comando = process.argv[2];
+const numeroA = process.argv[3];
+const numeroB = process.argv[4];
+
+switch (comando) {
+    case "sumar":
+        console.log(sumar(numeroA, numeroB))
+        break;
+    case "restar":
+        console.log(restar(numeroA, numeroB))
+        break;
+    case "multiplicar":
+        console.log(multiplicar(numeroA, numeroB))
+        break;
+    case "dividir":
+        console.log(dividir(numeroA, numeroB))
+        break;
+    case undefined:
+        console.log('Debes ingresar un comando')
+        break;                    
+    default:
+        console.log('El comando es incorrecto')
+        break;
+}
